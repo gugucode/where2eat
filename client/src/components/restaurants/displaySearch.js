4d68cfcdc70f5658e7778fa0b8c80b1130bdaf7e
gugucode <meiyuechang@gmail.com>
2018-09-18 09:16:33 -0500

    display result

--- client/src/components/restaurants/displaySearch.js
+++ client/src/components/restaurants/displaySearch.js
@@ -2,9 +2,9 @@ import React from "react";
 import { DisplayResults } from "./restDisplay";
 import { ShowRestList } from "../showRest/showRestList/showRestList";
 import API from "../../utils/API";
-import DeleteRest from "./savedRestaurant";
-import { RestSearch } from "./restSearch";
 
+import ShowResult from "./showResult";
+import "./restStyle.css";
 
 class DisplaySearch extends React.Component {
   constructor(props) {
@@ -16,7 +16,9 @@ class DisplaySearch extends React.Component {
         originRestArray: [],
         rest1: null,
         rest2: null,
-        restPickNum: {}
+        restPickNum: {},
+        progress: 0,
+        chose: null
       };
   }
 
@@ -33,61 +35,100 @@ class DisplaySearch extends React.Component {
     
       this.setState({ 
         changeArray: res.data,
-        originRestArray: res.data,
+        originRestArray: Array.of(...res.data),
       }, () => {
-        console.log(this.state.changeArray.length)
-        // console.log(this.state.rest1)
-        // console.log(this.state.rest2)
-        this.setState({
-          rest1: this.getRamdRest(),
-          rest2: this.getRamdRest()
-        })
+        this.setRest("rest1");
+        this.setRest("rest2");
       }
     )})
   }
 
-  getRamdRest = () => {
-    console.log(this.state.changeArray.length)
-    const index = Math.floor(Math.random()* this.state.changeArray.length)
-    const t = this.state.changeArray.splice(index,1)
-    console.log(t)
-    return t[0].restaurant;
-  }
-
-  setRest = (rest,id) => {
+  setRest = (rest) => {
     this.setState((previousState, currentProps)=>{
-      let restPickNum = previousState.restPickNum;
-      restPickNum[id] = restPickNum[id] ? restPickNum[id] + 1: 1;
       
+      const index = Math.floor(Math.random()* this.state.changeArray.length)
+      console.log(index)
+      const t = this.state.changeArray[index].restaurant;
+      let changeArray = previousState.changeArray;
+      changeArray.splice(index,1);
+
       return {
-        [rest]: this.getRamdRest(),
-        restPickNum: restPickNum
+        [rest]: t,
+        // restPickNum: restPickNum,
+        changeArray: changeArray,
+        progress: (1 - (changeArray.length / previousState.originRestArray.length)) * 100
       }
     })
   }
 
-  // When save restaurant button is clicked, add restaurant to db
+  // When save restaurant button is clicked, get new restaurant 
   handlePick = (id) => {
-    console.log(id);
-    console.log(this.state.restPickNum);
+
+    // Save number of pick for each restaurant
+    this.setState((previousState, currentProps)=>{
+      let restPickNum = previousState.restPickNum;
+      restPickNum[id] = restPickNum[id] ? restPickNum[id] + 1: 1;
+
+      return {
+        restPickNum: restPickNum,
+      }
+    });
 
     if(this.state.changeArray.length > 0){
-      id === this.state.rest1.id ?
-      (
-        this.setRest("rest2",id)
-      ) : (
-        this.setRest("rest1",id)
-      )
+      if(id === this.state.rest1.id)
+      {
+        this.setRest("rest2")
+       }else{
+        this.setRest("rest1")
+       }
+    }else{
+      if(id === this.state.rest1.id)
+      {
+        this.setState({
+          chose: this.state.rest1
+        })
+      }else{
+        this.setState({
+          chose: this.state.rest2
+        })
+      }
     }
   }
 
+loader = {
+    border: "16px solid #f3f3f3",
+    borderRadius: "50%",
+    borderTop: "16px solid #3498db",
+    width: "120px",
+    height: "120px",
+    animation: "spin 2s linear infinite"
+  }
 
   render() {
       return(
-          <div className="row justify-content-center" id="result">
-            { this.state.rest1 !== null ? <DisplayResults pickRest={this.handlePick} data={this.state.rest1} /> : ("") }
-            { this.state.rest2 !== null ? <DisplayResults pickRest={this.handlePick} data={this.state.rest2} /> : ("") }
-          </div>
+        
+          this.state.changeArray.length > 0 || this.state.chose === null ? 
+          (
+            <div>
+              <div className="progress">
+                <div className="progress-bar progress-bar-striped bg-info progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: `${this.state.progress}%`}}></div>
+              </div>
+              <div className="row justify-content-center" id="result">
+                { this.state.rest1 !== null ? <DisplayResults pickRest={this.handlePick} data={this.state.rest1} /> : ("") }
+                { this.state.rest2 !== null ? <DisplayResults pickRest={this.handlePick} data={this.state.rest2} /> : ("") }
+              </div>
+            </div>
+          ) : (
+            this.state.chose !== null ? 
+            (
+              <ShowResult chose={this.state.chose} others={this.state.originRestArray} vote={this.state.restPickNum}/> 
+            ): (
+              <div className="row justify-content-center">
+                <div className="loader m-5"></div>
+              </div>
+            )
+          )
+
       )
 
   }
